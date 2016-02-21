3c5996c735d282a0538092048514da492948f302
Angelo Matni <angelomatni2018@u.northwestern.edu>
2016-02-20 21:53:59 -0600

    quiz process table started

--- quizCreate.js
+++ quizCreate.js
@@ -106,7 +106,7 @@ function generateQuestions() {
 		generateQuestion(questionIds[i],answerProduct);
 	}
 	
-	storeQuizProcessingInfo(questionIds,correctAnswers)
+	storeQuizProcessingInfo(questionIds,answerProduct.ShortDescription,correctAnswers)
 }
 
 function generateQuestion(questionId,answerProduct) {
@@ -151,30 +151,4 @@ function shuffle(o) {
   }
 
   return o;
-}
-
-// Quiz process----------------------------------------------------------
-var questionIds;
-var rightAnswers;
-
-function processQuiz() {
-	var results = []; //A true or false for whether the question was answered correctly.
-	for (var i = 0; i < questionIds.length; i++) {
-		var question = document.getElementById(questionIds[i]);
-		var radios = question.getElementsById(questionIds[i]);
-		for (var j = 0; j < radios.length; j++) {
-			if (radios[j].checked) {
-				answer = radios[j].value;
-				if (rightAnswers[i] == answer) {
-					results[i] = true;
-				} else results[i] = false;
-			}
-		}
-	}
-	return results;
-}
-
-function storeQuizProcessingInfo(questions, ans) {
-	questionNames = questions;
-	rightAnswers = ans;
 }
--- quizProcess.js
+++ quizProcess.js
@@ -1,12 +1,40 @@
 var questionIds;
+var questions;
 var rightAnswers;
 
 function processQuiz() {
+	var results = checkAnswers();
+	var tableBody = document.createElement("quizResults");
+	for (i = 0; i < results.length; i++) {
+		var elementHolder = document.createElement("tr");
+		var elementIndex = document.createElement("td");
+		elementIndex.innerHTML = i;
+		var elementC = document.createElement("td");
+		elementC.innerHTML = results;
+		var elementQ = document.createElement("td");
+		elementQ.innerHTML = questions[i];
+		var elementA = document.createElement("td");
+		elementA.innerHTML = rightAnswers[i];
+		
+		elementHolder.appendChild(elementIndex);
+		elementHolder.appendChild(elementC);
+		elementHolder.appendChild(elementQ);
+		elementHolder.appendChild(elementA);
+	}
+}
+
+function storeQuizProcessingInfo(qIds, q, ans) {
+	questionIds = qIds;
+	questions = q;
+	rightAnswers = ans;
+}
+
+function checkAnswers() {
 	var results = []; //A true or false for whether the question was answered correctly.
 	for (var i = 0; i < questionIds.length; i++) {
 		var question = document.getElementById(questionIds[i]);
 		var radios = question.getElementsById(questionIds[i]);
-		for (int j = 0; j < radios.length; j++) {
+		for (j = 0; j < radios.length; j++) {
 			if (radios[j].checked) {
 				answer = radios[j].value;
 				if (rightAnswers[i] == answer) {
@@ -16,9 +44,4 @@ function processQuiz() {
 		}
 	}
 	return results;
-}
-
-function storeQuizProcessingInfo(questions, ans) {
-	questionNames = questions;
-	rightAnswers = ans;
 }
